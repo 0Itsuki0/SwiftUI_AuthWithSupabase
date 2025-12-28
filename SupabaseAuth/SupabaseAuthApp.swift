0adcbc3de9887bc9c05aa4436175ba674c174559
itsuki <itsuki.enjyoy@gmail.com>
2025-12-29 08:59:48 +0900

    Initial Commit

---
+++ SupabaseAuth/SupabaseAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupabaseAuthApp.swift
+//  SupabaseAuth
+//
+//  Created by Itsuki on 2025/12/29.
+//
+
+import SwiftUI
+
+@main
+struct SupabaseAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
